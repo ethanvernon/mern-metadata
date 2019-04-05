1ce39dedc0f56925acc5308f88591249a87d4a5b
Ethan Vernon <ethan.vernon@colorado.edu>
2019-04-05 12:09:40 +0800

    changes Description.js h2 to an h3

--- client/src/Description.js
+++ client/src/Description.js
@@ -2,18 +2,18 @@ import React, {Component} from "react";
 import "./App.css";
 
 export class Description extends Component {
-	
+
 	render() {
 		return (
 			<div className="description">
 				<div className="description-text">
-					<h2>User stories:</h2>
+					<h3>User stories:</h3>
 					<ol>
 						<li>I can submit a form object that includes a file upload.</li>
 						<li>The from file input field has the "name" attribute set to "upfile". We rely on this in testing.</li>
 						<li>When I submit something, I will receive the file name, type, and size in bytes within the JSON response.</li>
 					</ol>
-					<h2>Usage:</h2>
+					<h3>Usage:</h3>
 					<p>Please upload a file...</p>
 				</div>
 			</div>
